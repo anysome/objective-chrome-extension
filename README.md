275569f12e05b3b2dd53df6bb6ee5d5846d85ac5
anysome <anysome@gmail.com>
2016-10-25 21:36:50 +0800

    init project

---
+++ README.md
@@ -0,0 +1,17 @@
+# extension
+> A vue project.
+
+## Development
+
+```shell
+npm i cooking -g
+npm run dev
+```
+
+## Production
+```
+npm run dist
+```
+
+## License
+MIT
---
+++ cooking.conf.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var cooking = require('cooking');
+
+cooking.set({
+  entry: {
+    app: ['./src/main.js']
+  },
+  dist: './dist',
+  template: './index.html',
+
+  devServer: {
+    port: 8080,
+    publicPath: '/',
+    proxy: {
+      '/**/*.json': {
+        target: 'http://api.asfun.cn',
+        host: 'api.asfun.cn',
+        secure: false
+      }
+    }
+  },
+
+  // production
+  clean: true,
+  hash: false,
+  sourceMap: false,
+  minimize: true,
+  chunk: false, // see https://cookingjs.github.io/zh-cn/configuration.html#chunk
+  postcss: [
+    // require('...')
+  ],
+  publicPath: '/dist/',
+  assetsPath: 'assets',
+  urlLoaderLimit: 10000,
+  extractCSS: 'popup.css',
+  alias: {
+    'src': path.join(__dirname, 'src')
+  },
+  extends: ['lint', 'saladcss'],
+  format: 'umd'
+});
+
+module.exports = cooking.resolve();
